6b33ea5cc19e0e517a27d586495de0f829d0dd19
MAKAR190 <lutskyimakar@gmail.com>
2025-03-22 00:00:27 +0100

    finished eslint action

---
+++ eslint.config.js
@@ -0,0 +1,59 @@
+const { FlatCompat } = require("@eslint/eslintrc");
+const eslintPluginUnusedImports = require("eslint-plugin-unused-imports");
+const eslintPluginTypescript = require("@typescript-eslint/eslint-plugin");
+const typescriptParser = require("@typescript-eslint/parser");
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: {},
+});
+
+module.exports = [
+  ...compat.extends("@react-native-community"),
+  {
+    ignores: [
+      "node_modules", 
+      "dist",
+      "build",
+      ".expo",
+      "web-build",
+      ".vscode",
+      ".gitignore",
+      ".prettierrc",
+    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "@typescript-eslint": eslintPluginTypescript,
+      "unused-imports": eslintPluginUnusedImports,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      quotes: ["off"],
+      "eol-last": ["off"],
+      "react/react-in-jsx-scope": ["off"],
+      "no-trailing-spaces": ["off"],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          args: "all",
+          ignoreRestSiblings: false,
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "unused-imports/no-unused-imports": "error",
+      "@typescript-eslint/func-call-spacing": "off",
+      "prettier/prettier": "off",
+    },
+  },
+];
--- metro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { withNativeWind } from "nativewind/metro";
-import {
-  getSentryExpoConfig
-} from "@sentry/react-native/metro";
-
-const config = getSentryExpoConfig(__dirname);
-
-module.exports = withNativeWind(config, { input: "./global.css" });
